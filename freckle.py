a274c223e86dd142067a84e91c98f7772651bdfe
Joe Dougherty <jdougherty@contextllc.com>
2012-10-29 20:29:31 -0400

    created api class

--- freckle.py
+++ freckle.py
@@ -1,7 +1,21 @@
 import freckle_lib
+from ConfigParser import SafeConfigParser
+
+#open config file
+parser = SafeConfigParser()
+parser.read('.freckle')
+
+# Connection details
+base_url = parser.get('freckle_credentials', 'base_url') 
+
+# User details
+api_key  = parser.get('freckle_credentials', 'api_key')
+user     = parser.get('freckle_credentials', 'user')
+
+api_object = freckle_lib.FreckleApi(base_url, api_key)
 
 # load all projects
-all_projects = freckle_lib.get_all_projects()
+all_projects = api_object.get_all_projects()
 
 freckle_lib.print_welcome_message()
 freckle_lib.print_project_menu(all_projects)
@@ -11,3 +25,5 @@ current_project_id = freckle_lib.get_project_id()
 
 # store freckle project id 
 freckle_project_id = freckle_lib.get_freckle_project_id(all_projects, current_project_id)
+
+print freckle_project_id
--- freckle_lib.py
+++ freckle_lib.py
@@ -1,37 +1,40 @@
 import requests
 import json
-from ConfigParser import SafeConfigParser
 
 def print_welcome_message():
     print "\nFreckle CLI v0.1: The Luddite's Preferred Time Tracking Interface.\n"
     print "Select a project to work on. Choose wisely.\n"
     print "Loading projects..."
 
-def get_all_projects():
-    parser = SafeConfigParser()
+class FreckleApi(object):
+   def __init__(self, arg1, arg2):
+        self.base_url = arg1 
+        self.api_key  = arg2
+
+   def get_all_projects(self):
+        url = self.base_url + '/projects.json'
+        headers = {'X-FreckleToken': self.api_key} 
+
+        r = requests.get(url, headers=headers)
+        projects = r.json
+
+        count = 0
     
-    try: 
-        parser.read('.freckle')
-    except IOError:
-        print 'Could not detect .freckle configuration file.'
+        # Include our own index in each project dictionary
+        for project in projects:
+            project[u'project'][u'cli_id'] = count
+            count += 1
 
-    base_url = parser.get('freckle_credentials', 'base_url')
-    freckle_key = parser.get('freckle_credentials', 'api_key')
-    url = base_url + '/projects.json'
-    
-    headers = {'X-FreckleToken': freckle_key} 
+        return projects
 
-    r = requests.get(url, headers=headers)
-    projects = r.json
+   def create_time_entry(self, entry_xml):
+        url = self.base_url + '/entries.xml'
+        headers = {"X-FreckleToken" : "lltpffanobj4a8yzpihmm7xan0al89s", "Content-type" : "text/xml"}
+        
+        files = {'file': entry_xml}
 
-    count = 0
-    
-    # Include our own index in each project dictionary
-    for project in projects:
-        project[u'project'][u'cli_id'] = count
-        count += 1
-
-    return projects
+        r = requests.post(url, headers=headers, files=files) 
+        return
 
 def get_project_id():
     project_id = str(input("\nEnter Project#: "))
@@ -48,3 +51,20 @@ def get_freckle_project_id(projects_object, cli_id):
     for project in projects_object:
         if project[u'project'][u'cli_id'] == int(cli_id):
             return project[u'project'][u'id']
+
+def generate_xml_post(minutes, user, project_num, tags='development'):    
+    minutes = int(minutes)
+    # convert back to str and append 'm'
+    minutes = str(minutes) + 'm' 
+    project_num = str(project_num)
+
+    xml_post = '''
+                <?xml version="1.0" encoding="UTF-8"?>
+                    <entry>
+                        <minutes>''' + minutes + '''</minutes>
+                        <user>''' + user + '''</user>
+                        <project-id type="integer">''' + project_num  + '''</project-id>
+                        <description>''' + tags + '''</description>
+                    </entry>
+                '''
+    return xml_post           
